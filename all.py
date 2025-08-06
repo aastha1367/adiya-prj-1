574f4b9530d72edc24b605518172e807573c2739
aastha1367 <aastha130607@gmail.com>
2025-08-06 16:18:52 +0530

    Add files via upload

---
+++ all.py
@@ -0,0 +1,22 @@
+for i in range(1, 6):
+    print(" " * (6 - i), end=" ")
+    print("* " * i)
+
+for i in range(4, 0,-1):
+    print(" " * (6 - i), end=" ")
+    print("* " * i)
+    
+for i in range(1,6):
+    print(" " * (6-i), end=" ")
+    print("* " * i)
+for i in range(1,1+5):
+    for j in range(1,i+1):
+        print(j,end=" ")
+    print()
+
+for i in range(1, 6):
+    print("  " * (5 - i), end="")  
+    for j in range(1, i + 1):
+        print(j, end=" ")
+    print()
+
